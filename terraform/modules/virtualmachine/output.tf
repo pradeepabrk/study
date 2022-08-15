6447796725d4fe623adcc93190aa98b68d8fb83e
pradeepa <pradeepa.b.rk@gmail.com>
2022-08-15 11:30:28 +0100

    modified ip value

--- terraform/modules/virtualmachine/output.tf
+++ terraform/modules/virtualmachine/output.tf
@@ -7,6 +7,6 @@ value = google_compute_instance.default.instance_id
 
 output "vm-ip" {
 
-value = google_compute_instance.default.network_interface.network_ip
+value = google_compute_instance.default.network_interface.0.network_ip
 
 }
