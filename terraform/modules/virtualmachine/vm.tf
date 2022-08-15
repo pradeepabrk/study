8d4be1e53e52639a12783a6c68ae4e16d7e77523
pradeepa <pradeepa.b.rk@gmail.com>
2022-08-15 12:24:40 +0100

    unique name for vms

--- terraform/modules/virtualmachine/vm.tf
+++ terraform/modules/virtualmachine/vm.tf
@@ -1,5 +1,6 @@
 resource "google_compute_instance" "default" {
-  name         = "test"
+  count = 3
+  name         = element(var.vmname, count.index)
   machine_type = "e2-micro"
   allow_stopping_for_update = true
 
