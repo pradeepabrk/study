0935450c0caea8c0aa30e137095e6d8fc53cf5b4
pradeepa <pradeepa.b.rk@gmail.com>
2022-08-15 11:25:56 +0100

    modified output attribute

--- terraform/modules/virtualmachine/output.tf
+++ terraform/modules/virtualmachine/output.tf
@@ -1,6 +1,12 @@
 output "vm-id" {
 
-value = google_compute_instance.default.id
+value = google_compute_instance.default.instance_id
 
 }
 
+
+output "vm-ip" {
+
+value = google_compute_instance.default.network_interface.network_ip
+
+}
